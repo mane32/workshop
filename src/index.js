437961d48c9c097d2ef46c54b93a11a809674f53
vannesa <manelp3214@gmail.com>
2022-01-27 22:40:44 -0600

    Terminamos video 1

--- src/index.js
+++ src/index.js
@@ -6,11 +6,27 @@
 const url = "https://platzi-avo.vercel.app/api/avo";
 //web api
 //conectarnos al servidos
-window.fetch(url)
+window
+.fetch(url)
 //procesar la respuesta y convertirla en JSON 
-.then(respuesta => respuesta.json())
+.then((respuesta) => respuesta.json())
 //JSON ->Data 
-.then(data => {
-   console.log(data) 
-})
+.then((responseJson) => {
+    const todosLosItems = [];
+  responseJson.data.forEach((item) => {
+      //crear imagen
+      const imagen = document.createElement("img");
+
+      //crear titulo
+      const title = document.createElement("h2");
+      //crear precio
+      const price = document.createElement("div");
+
+      const container = document.createElement("div");
+      container.append(imagen, title, price);
+
+      todosLosItems.push(container);
+  });
+  document.body.append(...todosLosItems);
+});
 
