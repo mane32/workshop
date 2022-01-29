5b8714e69d60d8332f14cb7a798ffa214189dc5d
vannesa <manelp3214@gmail.com>
2022-01-28 22:38:27 -0600

    Terminamos todo

--- src/index.js
+++ src/index.js
@@ -27,22 +27,33 @@ window
       //crear imagen
       const imagen = document.createElement("img");
       imagen.src = `${baseUrl}${item.image}`;
-
+      
       //crear titulo
       const title = document.createElement("h2");
       title.textContent = item.name;
       //title.style = "font-size: 2rem";
-      title.className = "text-5xl text-pink-600";
+      title.className = "text-5xl text-green-600 font-style: italic text-decoration-line: underline"
+      title.textContent = item.name;
+
       //crear precio
       const price = document.createElement("div");
-      price.className = "text-gray-600";
+      price.className = "text-gray-600 text-xl";
       price.textContent = formatPrice(item.price);
+//unimos precio y titulo
+const priceAndTitle = document.createElement('div');
+priceAndTitle.className = "text-center md: text-left";
+priceAndTitle.appendChild(title);
+priceAndTitle.appendChild(price);
 
-      const container = document.createElement("div");
-      container.append(imagen, title, price);
+//unimos imagen con priceAndTitle
+const card = document.createElement('div');
+card.className = "md:flex bg-white rounded-lg p-6";
+card.appendChild(imagen);
+card.appendChild(priceAndTitle);
 
-      todosLosItems.push(container);
-  });
-  appNode.append(...todosLosItems);
+todosLosItems.push(card);
 });
+  appNode.append(...todosLosItems);
 
+
+});
