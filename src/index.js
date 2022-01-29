2b16e226efe8536c3630d167508b75d84a4f0459
vannesa <manelp3214@gmail.com>
2022-01-28 20:30:20 -0600

    creamos imagenes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
+ const baseUrl = "https://platzi-avo.vercel.app";
 
-const url = "https://platzi-avo.vercel.app/api/avo";
 //web api
 //conectarnos al servidos
 window
-.fetch(url)
+.fetch(`${baseUrl}/api/avo`)
 //procesar la respuesta y convertirla en JSON 
 .then((respuesta) => respuesta.json())
 //JSON ->Data 
@@ -16,6 +16,7 @@ window
   responseJson.data.forEach((item) => {
       //crear imagen
       const imagen = document.createElement("img");
+      imagen.src = `${baseUrl}${item.image}`;
 
       //crear titulo
       const title = document.createElement("h2");
